710a3b2f737fd3a32394d0b5ce4660f07497083e
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-11 17:19:52 +0300

    Separate Javascript files

---
+++ hanburger.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+      // if any scroll is attempted,
+      // set this to the previous value
+      (window.onscroll = function () {
+        window.scrollTo(scrollLeft, scrollTop);
+      });
+  }
+  function enableScroll() {
+    window.onscroll = function () {};
+  }
+
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+
+  menu_btn.addEventListener("click", () => {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+    // disableScroll();
+  });
+};
--- main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  const menu_btn = document.querySelector(".hamburger");
-  const mobile_menu = document.querySelector(".mobile-nav");
-
-  menu_btn.addEventListener("click", () => {
-    menu_btn.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
-  });
-};
