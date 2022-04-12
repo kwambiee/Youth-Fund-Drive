80f01286bfa02f3bb3e079997c415f6563cd9fed
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-12 13:50:16 +0300

    Adapt to desktop version

--- hanburger.js
+++ hanburger.js
@@ -19,6 +19,5 @@ window.onload = function () {
   menu_btn.addEventListener("click", () => {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
-    // disableScroll();
   });
 };
