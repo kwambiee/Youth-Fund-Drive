c4844d8d0a1f5fe4a23408ea7a23506bd6949cde
Kwamboka <kwambokaj2.jk@gmail.com>
2022-04-13 08:49:24 +0300

    Add interactions to mobile menu

--- about.js
+++ about.js
@@ -13,11 +13,10 @@ window.onload = function () {
     window.onscroll = function () {};
   }
 
-  const menu_btn = document.getElementById("hamburger");
+  const menu_btn = document.querySelector(".hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
 
   menu_btn.addEventListener("click", (e) => {
-    console.log(e);
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
